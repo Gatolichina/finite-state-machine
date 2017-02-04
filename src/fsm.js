55f08455b5c58223dda2fc8435f3a12fe3a245dd
Gatolichina <antonchikg@gmail.com>
2017-02-04 21:34:37 +0300

    FiniteStateMachine: delete his.js

--- src/fsm.js
+++ src/fsm.js
@@ -1,4 +1,17 @@
-const His = require('./his');
+class His {
+    constructor(state) {
+        this.action=[state];
+        this.present=0;
+        this.head = 0;
+    }
+
+    adding(state){
+      this.action.push(state);
+      this.present+=1;
+      this.head+=1;
+    }
+}
+
 class FSM {
 
 
